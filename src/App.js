90f05478d02a8fcf7bc498317942c4e85f152a81
Gurden Batra <gurden.batra@gmail.com>
2022-05-25 15:48:03 +0300

    Adding needed config for router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import SubmitProject from "./pages/SubmitProject";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/submit-project" element={<SubmitProject />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
