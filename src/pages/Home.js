ba8dd4f2211167136653412555a0510462201499
Gurden Batra <gurden.batra@gmail.com>
2022-05-25 15:11:29 +0300

    finish home page components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,30 @@
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import map from "../images/map.png";
+import ecosystem from "../images/ecosystem.png";
+
+const cities = [
+  {
+    name: "Dublin",
+    imageUrl: "assets/cities/Dublin.png",
+  },
+  {
+    name: "London",
+    imageUrl: "assets/cities/London.png",
+  },
+  {
+    name: "Paris",
+    imageUrl: "assets/cities/Paris.png",
+  },
+  {
+    name: "Milan",
+    imageUrl: "assets/cities/Milan.png",
+  },
+  {
+    name: "Berlin",
+    imageUrl: "assets/cities/Berlin.png",
+  },
+];
 
 export default function Home() {
   return (
@@ -111,6 +135,69 @@ export default function Home() {
                           className="inline-flex items-center px-4 py-2 text-base font-spaceRegular font-large rounded-full text-white bg-primary hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         >
                           View detailed portfolio
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bg-primary">
+              <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+                <h2 className="text-4xl font-extrabold tracking-tight sm:text-4xl font-spaceBold text-white">
+                  <span className="block">Other cities coming soon!</span>
+                </h2>
+                <div className="space-y-12 m-10">
+                  <ul
+                    role="list"
+                    className="mx-auto sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:grid-cols-5 lg:max-w-8xl"
+                  >
+                    {cities.map((city) => (
+                      <li key={city.name}>
+                        <div className="space-y-6">
+                          <img
+                            className="mx-auto h-30 w-40 rounded-full"
+                            src={city.imageUrl}
+                            alt=""
+                          />
+                          <div className="space-y-2">
+                            <div className="text-lg leading-6 font-medium space-y-1">
+                              <p className="text-white font-spaceRegular">
+                                {city.name}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="bg-background">
+              <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+                <h2 className="text-4xl font-extrabold tracking-tight sm:text-4xl font-spaceBold text-primary">
+                  <span className="block">TreesAI ecosystem of products</span>
+                </h2>
+                <div className="md:grid-cols-2 lg:grid lg:grid-cols-2 sm:grid-cols-1">
+                  <div className="m-5">
+                    <img src={ecosystem} />
+                  </div>
+                  <div>
+                    <p className="font-spaceRegular text-md text-left mt-5">
+                      TreesAI ecosystem of products is an easy to use map based
+                      interface that helps project developers exchange valuable
+                      design information like location and impact so that they
+                      can get funded as portfolios rather than on a
+                      project-by-project basis.
+                    </p>
+                    <div className="mt-8 flex justify-center">
+                      <div className="inline-flex rounded-md">
+                        <a
+                          href="#"
+                          className="inline-flex items-center px-4 py-2 text-base font-spaceRegular font-large rounded-full text-white bg-primary hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                          Learn more
                         </a>
                       </div>
                     </div>
