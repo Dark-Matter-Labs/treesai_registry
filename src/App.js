dae77668e9be9d884e778a97be9169cc4d76e043
Gurden Batra <gurden.batra@gmail.com>
2022-06-06 16:48:43 +0300

    adding blank page for portfolio page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 import Home from "./pages/Home";
 import SubmitProject from "./pages/SubmitProject";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/submit-project" element={<SubmitProject />} />
+        <Route exact path="/portfolio" element={<Portfolio />} />
       </Routes>
     </Router>
   );
