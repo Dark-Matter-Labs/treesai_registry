82a0d8af020f021fdc03b1314c96eb17e20191e9
Gurden Batra <gurden.batra@gmail.com>
2022-06-21 16:41:45 +0300

    Code cleanup via Prettier

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,12 @@
 import ReactMapboxGL, { Source, Layer } from "react-map-gl";
-import mapboxgl from "mapbox-gl"
+import mapboxgl from "mapbox-gl";
 import { useState, useCallback } from "react";
 import GlasgowJSON from "../data/VDL_selected.geojson";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
-mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+mapboxgl.workerClass =
+  require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_KEY;
 
@@ -22,43 +25,47 @@ export default function Portfolio() {
   }, []);
 
   return (
-    <ReactMapboxGL
-      mapboxAccessToken={MAPBOX_TOKEN}
-      style={{ width: 2000, height: 1000 }}
-      mapStyle="mapbox://styles/mapbox/streets-v11"
-      interactiveLayerIds={["my-data"]}
-      onMouseMove={onHover}
-      initialViewState={{
-        longitude: -4.2518,
-        latitude: 55.8642,
-        zoom: 12,
-      }}
-    >
-      <Source id="my-data" type="geojson" data={GlasgowJSON}>
-        <Layer
-          id="my-data"
-          type="fill"
-          layout={{}}
-          paint={{ "fill-color": "#10B981", "fill-opacity": 1 }}
-        />
-        <Layer
-          id="outline"
-          type="line"
-          layout={{}}
-          paint={{ "line-color": "#000", "line-width": 2 }}
-        />
-      </Source>
-      {hoverInfo && (
-        <div
-          className="tooltip"
-          style={{ left: hoverInfo.x, top: hoverInfo.y }}
-        >
-          <div>Name: {hoverInfo.feature.properties.NAME}</div>
-          <div>Site code: {hoverInfo.feature.properties.SITECODE}</div>
-          <div>Ward: {hoverInfo.feature.properties.WARD}</div>
-          <div>Area in Ha: {hoverInfo.feature.properties.Area_Hecta}</div>
-        </div>
-      )}
-    </ReactMapboxGL>
+    <>
+      <NavBar />
+      <ReactMapboxGL
+        mapboxAccessToken={MAPBOX_TOKEN}
+        style={{ width: "100vw", height: "80vh" }}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        interactiveLayerIds={["my-data"]}
+        onMouseMove={onHover}
+        initialViewState={{
+          longitude: -4.2518,
+          latitude: 55.8642,
+          zoom: 12,
+        }}
+      >
+        <Source id="my-data" type="geojson" data={GlasgowJSON}>
+          <Layer
+            id="my-data"
+            type="fill"
+            layout={{}}
+            paint={{ "fill-color": "#10B981", "fill-opacity": 1 }}
+          />
+          <Layer
+            id="outline"
+            type="line"
+            layout={{}}
+            paint={{ "line-color": "#000", "line-width": 2 }}
+          />
+        </Source>
+        {hoverInfo && (
+          <div
+            className="tooltip"
+            style={{ left: hoverInfo.x, top: hoverInfo.y }}
+          >
+            <div>Name: {hoverInfo.feature.properties.NAME}</div>
+            <div>Site code: {hoverInfo.feature.properties.SITECODE}</div>
+            <div>Ward: {hoverInfo.feature.properties.WARD}</div>
+            <div>Area in Ha: {hoverInfo.feature.properties.Area_Hecta}</div>
+          </div>
+        )}
+      </ReactMapboxGL>
+      <Footer />
+    </>
   );
 }
