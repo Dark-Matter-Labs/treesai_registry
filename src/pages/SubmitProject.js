0f9deebe31d0e499a49f559e87fb97a3fbf72afa
Gurden Batra <gurden.batra@gmail.com>
2022-06-22 13:53:53 +0300

    navigate away from project submission if user is not logged in(need to make notification component global)

--- src/pages/SubmitProject.js
+++ src/pages/SubmitProject.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
-import { RadioGroup } from "@headlessui/react";
-import { CheckCircleIcon } from "@heroicons/react/solid";
+import { useState, useEffect, Fragment } from "react";
+import { RadioGroup, Transition } from "@headlessui/react";
+import { CheckCircleIcon, XIcon } from "@heroicons/react/solid";
 import { Line } from "@nivo/line";
 import { ResponsiveBar } from "@nivo/bar";
+import { useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
@@ -103,6 +104,7 @@ function sumRange(array, start, end) {
 
 export default function SubmitProject() {
   const [processStage, setProcessStage] = useState(1);
+  const [showNotification, setShowNotification] = useState(false);
 
   const [projectName, setProjectName] = useState("");
   const [projectDescription, setProjectDescription] = useState("");
@@ -110,6 +112,15 @@ export default function SubmitProject() {
   const [minDBH, setMinDBH] = useState(0);
   const [maxDBH, setMaxDBH] = useState(0);
   const [areaDensity, setAreaDensity] = useState(0);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (sessionStorage.getItem("token") === null) {
+      setShowNotification(true);
+      navigate("/register");
+    }
+  });
 
   const handleMinChange = (e) => {
     setMinDBH(e.target.value);
@@ -169,10 +180,7 @@ export default function SubmitProject() {
     requestHeaders.append("accept", "application/json");
     requestHeaders.append("Content-Type", "application/json");
     requestHeaders.append("Access-Control-Allow-Origin", "*");
-    requestHeaders.append(
-      "Authorization",
-      "Bearer " + sessionStorage.token
-    );
+    requestHeaders.append("Authorization", "Bearer " + sessionStorage.token);
 
     const payload = JSON.stringify({
       name: projectName,
@@ -268,7 +276,60 @@ export default function SubmitProject() {
 
   return (
     <>
-      <NavBar />
+      <>
+        <div
+          aria-live="assertive"
+          className="fixed inset-0 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start"
+        >
+          <div className="w-full flex flex-col items-center space-y-4 sm:items-end">
+            {/* TODO: make notifications global */}
+            <Transition
+              show={showNotification}
+              as={Fragment}
+              enter="transform ease-out duration-300 transition"
+              enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+              enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden">
+                <div className="p-4">
+                  <div className="flex items-start">
+                    <div className="flex-shrink-0">
+                      <CheckCircleIcon
+                        className="h-6 w-6 text-green-400"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="ml-3 w-0 flex-1 pt-0.5">
+                      <p className="text-sm font-medium text-gray-900">
+                        Please register or login first!
+                      </p>
+                      <p className="mt-1 text-sm text-gray-500">
+                        Taking you register page.
+                      </p>
+                    </div>
+                    <div className="ml-4 flex-shrink-0 flex">
+                      <button
+                        type="button"
+                        className="bg-white rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={() => {
+                          setShow(false);
+                        }}
+                      >
+                        <span className="sr-only">Close</span>
+                        <XIcon className="h-5 w-5" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Transition>
+          </div>
+        </div>
+      </>
+      <NavBar current="projectSubmit" />
       {processStage === 1 && (
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <form className="space-y-8 divide-y divide-gray-200">
