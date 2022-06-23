409a20a202e24d0583cf6cbbc26a5f32cb0a849f
Gurden Batra <gurden.batra@gmail.com>
2022-06-23 16:46:47 +0300

    adding token undefined check, error on API failure

--- src/pages/SubmitProject.js
+++ src/pages/SubmitProject.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, Fragment } from "react";
 import { RadioGroup, Transition } from "@headlessui/react";
 import { CheckCircleIcon, XIcon } from "@heroicons/react/solid";
+import { ExclamationCircleIcon } from "@heroicons/react/outline";
 import { Line } from "@nivo/line";
 import { ResponsiveBar } from "@nivo/bar";
 import { useNavigate } from "react-router-dom";
@@ -105,6 +106,7 @@ function sumRange(array, start, end) {
 export default function SubmitProject(props) {
   const [processStage, setProcessStage] = useState(1);
   const [showNotification, setShowNotification] = useState(false);
+  const [showError, setShowError] = useState(false);
 
   const [projectName, setProjectName] = useState("");
   const [projectDescription, setProjectDescription] = useState("");
@@ -116,7 +118,10 @@ export default function SubmitProject(props) {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (sessionStorage.getItem("token") === null) {
+    if (
+      sessionStorage.getItem("token") === null ||
+      sessionStorage.getItem("token") === undefined
+    ) {
       setShowNotification(true);
       navigate("/register");
     }
@@ -166,7 +171,13 @@ export default function SubmitProject(props) {
         "/projects",
       requestOptions
     )
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        setShowError(true);
+        throw new Error("Something went wrong");
+      })
       .then((result) => {
         sessionStorage.setItem("project_id", JSON.stringify(result.id));
 
@@ -211,7 +222,13 @@ export default function SubmitProject(props) {
         "/run",
       requestOptions
     )
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        setShowError(true);
+        throw new Error("Something went wrong");
+      })
       .then((result) => {
         console.log(result);
         avg_rel_array = Object.keys(result.Avg_Rel).map((key) => ({
@@ -316,6 +333,59 @@ export default function SubmitProject(props) {
                         className="bg-white rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         onClick={() => {
                           setShow(false);
+                        }}
+                      >
+                        <span className="sr-only">Close</span>
+                        <XIcon className="h-5 w-5" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Transition>
+          </div>
+        </div>
+      </>
+      <>
+        <div
+          aria-live="assertive"
+          className="fixed inset-0 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start"
+        >
+          <div className="w-full flex flex-col items-center space-y-4 sm:items-end pt-20">
+            {/* TODO: make notifications global */}
+            <Transition
+              show={showError}
+              as={Fragment}
+              enter="transform ease-out duration-300 transition"
+              enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+              enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden">
+                <div className="p-4">
+                  <div className="flex items-start">
+                    <div className="flex-shrink-0">
+                      <ExclamationCircleIcon
+                        className="h-6 w-6 text-red-400"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="ml-3 w-0 flex-1 pt-0.5">
+                      <p className="text-sm font-medium text-gray-900">
+                        Sorry there was an error!
+                      </p>
+                      <p className="mt-1 text-sm text-gray-500">
+                        error details
+                      </p>
+                    </div>
+                    <div className="ml-4 flex-shrink-0 flex">
+                      <button
+                        type="button"
+                        className="bg-white rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={() => {
+                          setShowError(false);
                         }}
                       >
                         <span className="sr-only">Close</span>
