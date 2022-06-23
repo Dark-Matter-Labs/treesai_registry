9b371037f9572806ed37676b398d881f92dc09f0
Gurden Batra <gurden.batra@gmail.com>
2022-06-23 16:45:46 +0300

    Adding check for error and showing notification

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,9 @@
 import { Fragment, useState } from "react";
 import { Transition } from "@headlessui/react";
-import { CheckCircleIcon } from "@heroicons/react/outline";
+import {
+  CheckCircleIcon,
+  ExclamationCircleIcon,
+} from "@heroicons/react/outline";
 import { XIcon } from "@heroicons/react/solid";
 import { useNavigate, Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
@@ -11,7 +14,8 @@ export default function Register() {
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-  const [show, setShow] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showError, setShowError] = useState(false);
   const navigate = useNavigate();
 
   const registerUser = async () => {
@@ -34,7 +38,13 @@ export default function Register() {
     };
 
     await fetch("http://127.0.0.1:8000/api/v1/users", createUserRequestOptions)
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        setShowError(true);
+        throw new Error("Something went wrong");
+      })
       .then((result) => {
         sessionStorage.setItem("user_id", JSON.stringify(result.id));
 
@@ -66,14 +76,20 @@ export default function Register() {
         };
 
         fetch("http://127.0.0.1:8000/api/v1/token", getTokenRequestOptions)
-          .then((response) => response.json())
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+            throw new Error("Something went wrong");
+          })
           .then((result) => {
+            console.log(result);
             sessionStorage.setItem(
               "token",
               JSON.stringify(result.access_token)
             );
 
-            setShow(true);
+            setShowSuccess(true);
             navigate("/submit-project");
             window.location.reload();
           })
@@ -92,7 +108,7 @@ export default function Register() {
           <div className="w-full flex flex-col items-center space-y-4 sm:items-end">
             {/* TODO: make notifications global */}
             <Transition
-              show={show}
+              show={showSuccess}
               as={Fragment}
               enter="transform ease-out duration-300 transition"
               enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
@@ -123,7 +139,60 @@ export default function Register() {
                         type="button"
                         className="bg-white rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         onClick={() => {
-                          setShow(false);
+                          setShowSuccess(false);
+                        }}
+                      >
+                        <span className="sr-only">Close</span>
+                        <XIcon className="h-5 w-5" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Transition>
+          </div>
+        </div>
+      </>
+      <>
+        <div
+          aria-live="assertive"
+          className="fixed inset-0 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start"
+        >
+          <div className="w-full flex flex-col items-center space-y-4 sm:items-end pt-20">
+            {/* TODO: make notifications global */}
+            <Transition
+              show={showError}
+              as={Fragment}
+              enter="transform ease-out duration-300 transition"
+              enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+              enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden">
+                <div className="p-4">
+                  <div className="flex items-start">
+                    <div className="flex-shrink-0">
+                      <ExclamationCircleIcon
+                        className="h-6 w-6 text-red-400"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="ml-3 w-0 flex-1 pt-0.5">
+                      <p className="text-sm font-medium text-gray-900">
+                        Sorry there was an error!
+                      </p>
+                      <p className="mt-1 text-sm text-gray-500">
+                        error details
+                      </p>
+                    </div>
+                    <div className="ml-4 flex-shrink-0 flex">
+                      <button
+                        type="button"
+                        className="bg-white rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={() => {
+                          setShowError(false);
                         }}
                       >
                         <span className="sr-only">Close</span>
