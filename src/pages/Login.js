ccc6a9268f52f4d2326d9eab3e519232574f467c
Gurden Batra <gurden.batra@gmail.com>
2022-06-23 16:45:38 +0300

    Adding check for error and showing notification, retrieve and set user ID

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,9 @@
 import { Fragment, useState } from "react";
 import { Transition } from "@headlessui/react";
-import { CheckCircleIcon } from "@heroicons/react/outline";
+import {
+  CheckCircleIcon,
+  ExclamationCircleIcon,
+} from "@heroicons/react/outline";
 import { XIcon } from "@heroicons/react/solid";
 import { useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar";
@@ -10,7 +13,8 @@ import logo from "../images/logo.svg";
 export default function Login() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-  const [show, setShow] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showError, setShowError] = useState(false);
   const navigate = useNavigate();
 
   const loginUser = async () => {
@@ -42,13 +46,47 @@ export default function Login() {
     };
 
     await fetch("http://127.0.0.1:8000/api/v1/token", getTokenRequestOptions)
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        setShowError(true);
+        throw new Error("Something went wrong");
+      })
       .then((result) => {
-        sessionStorage.setItem("token", JSON.stringify(result.access_token));
+        sessionStorage.setItem("token", result.access_token);
 
-        setShow(true);
-        navigate("/submit-project");
-        window.location.reload();
+        const getUserRequestHeaders = new Headers();
+        getUserRequestHeaders.append("accept", "application/json");
+        getUserRequestHeaders.append("Content-Type", "application/json");
+        getUserRequestHeaders.append("Access-Control-Allow-Origin", "*");
+        getUserRequestHeaders.append(
+          "Authorization",
+          "Bearer " + sessionStorage.token
+        );
+
+        const getUserRequestOptions = {
+          method: "GET",
+          headers: getUserRequestHeaders,
+          redirect: "follow",
+        };
+
+        fetch("http://127.0.0.1:8000/api/v1/users/me/", getUserRequestOptions)
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+            setShowError(true);
+            throw new Error("Something went wrong");
+          })
+          .then((result) => {
+            sessionStorage.setItem("user_id", JSON.stringify(result.id));
+            sessionStorage.setItem("user_name", JSON.stringify(result.name));
+            setShowSuccess(true);
+            navigate("/submit-project");
+            window.location.reload();
+          })
+          .catch((error) => console.log("error", error));
       })
       .catch((error) => console.log("error", error));
   };
@@ -63,7 +101,7 @@ export default function Login() {
           <div className="w-full flex flex-col items-center space-y-4 sm:items-end">
             {/* TODO: make notifications global */}
             <Transition
-              show={show}
+              show={showSuccess}
               as={Fragment}
               enter="transform ease-out duration-300 transition"
               enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
@@ -94,7 +132,60 @@ export default function Login() {
                         type="button"
                         className="bg-white rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         onClick={() => {
-                          setShow(false);
+                          setShowSuccess(false);
+                        }}
+                      >
+                        <span className="sr-only">Close</span>
+                        <XIcon className="h-5 w-5" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Transition>
+          </div>
+        </div>
+      </>
+      <>
+        <div
+          aria-live="assertive"
+          className="fixed inset-0 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start"
+        >
+          <div className="w-full flex flex-col items-center space-y-4 sm:items-end pt-20">
+            {/* TODO: make notifications global */}
+            <Transition
+              show={showError}
+              as={Fragment}
+              enter="transform ease-out duration-300 transition"
+              enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+              enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden">
+                <div className="p-4">
+                  <div className="flex items-start">
+                    <div className="flex-shrink-0">
+                      <ExclamationCircleIcon
+                        className="h-6 w-6 text-red-400"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="ml-3 w-0 flex-1 pt-0.5">
+                      <p className="text-sm font-medium text-gray-900">
+                        Sorry there was an error!
+                      </p>
+                      <p className="mt-1 text-sm text-gray-500">
+                        error details
+                      </p>
+                    </div>
+                    <div className="ml-4 flex-shrink-0 flex">
+                      <button
+                        type="button"
+                        className="bg-white rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={() => {
+                          setShowError(false);
                         }}
                       >
                         <span className="sr-only">Close</span>
