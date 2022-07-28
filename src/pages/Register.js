2ebf508614ca88d6f5d833556ca0e32d8191c95b
Gurden Batra <gurden.batra@gmail.com>
2022-07-28 14:34:25 +0300

    Remove Helmet test, add meta tag globally again

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,5 @@
 import React, { Fragment, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import logo from '../images/logo.svg';
@@ -93,98 +92,95 @@ export default function Register() {
 
   return (
     <>
-      <Helmet>
-        <meta httpEquiv='Content-Security-Policy' content='upgrade-insecure-requests' />
-        </Helmet>
-        <NavBar />
-        <div className='min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
-          <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-            <img className='mx-auto h-12 w-auto' src={logo} alt='TreesAI logo' />
-            <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>Register</h2>
-            <p className='text-center mt-2 font-small text-gray-900'>
-              Already registered? Login{' '}
-              <Link className='text-primary' to='/login'>
-                here
-              </Link>
-              .
-            </p>
-          </div>
-          <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
-            <div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
-              <form className='space-y-6'>
-                <div>
-                  <label htmlFor='name' className='block text-sm font-medium text-gray-700'>
-                    Name
-                  </label>
-                  <div className='mt-1'>
-                    <input
-                      id='name'
-                      name='name'
-                      type='text'
-                      defaultValue={name}
-                      onChange={(e) => {
-                        setName(e.target.value);
-                      }}
-                      required
-                      className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                    />
-                  </div>
+      <NavBar />
+      <div className='min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+          <img className='mx-auto h-12 w-auto' src={logo} alt='TreesAI logo' />
+          <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>Register</h2>
+          <p className='text-center mt-2 font-small text-gray-900'>
+            Already registered? Login{' '}
+            <Link className='text-primary' to='/login'>
+              here
+            </Link>
+            .
+          </p>
+        </div>
+        <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+          <div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
+            <form className='space-y-6'>
+              <div>
+                <label htmlFor='name' className='block text-sm font-medium text-gray-700'>
+                  Name
+                </label>
+                <div className='mt-1'>
+                  <input
+                    id='name'
+                    name='name'
+                    type='text'
+                    defaultValue={name}
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                    required
+                    className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                  />
                 </div>
+              </div>
 
-                <div>
-                  <label htmlFor='email' className='block text-sm font-medium text-gray-700'>
-                    Email address
-                  </label>
-                  <div className='mt-1'>
-                    <input
-                      id='email'
-                      name='email'
-                      type='email'
-                      autoComplete='email'
-                      defaultValue={email}
-                      onChange={(e) => {
-                        setEmail(e.target.value);
-                      }}
-                      required
-                      className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                    />
-                  </div>
+              <div>
+                <label htmlFor='email' className='block text-sm font-medium text-gray-700'>
+                  Email address
+                </label>
+                <div className='mt-1'>
+                  <input
+                    id='email'
+                    name='email'
+                    type='email'
+                    autoComplete='email'
+                    defaultValue={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                    required
+                    className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                  />
                 </div>
+              </div>
 
-                <div>
-                  <label htmlFor='password' className='block text-sm font-medium text-gray-700'>
-                    Password
-                  </label>
-                  <div className='mt-1'>
-                    <input
-                      id='password'
-                      name='password'
-                      type='password'
-                      defaultValue={password}
-                      onChange={(e) => {
-                        setPassword(e.target.value);
-                      }}
-                      required
-                      className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                    />
-                  </div>
+              <div>
+                <label htmlFor='password' className='block text-sm font-medium text-gray-700'>
+                  Password
+                </label>
+                <div className='mt-1'>
+                  <input
+                    id='password'
+                    name='password'
+                    type='password'
+                    defaultValue={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    required
+                    className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                  />
                 </div>
+              </div>
 
-                <div>
-                  <button
-                    onClick={registerUser}
-                    type='button'
-                    className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                  >
-                    Register
-                  </button>
-                </div>
-              </form>
-            </div>
+              <div>
+                <button
+                  onClick={registerUser}
+                  type='button'
+                  className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                >
+                  Register
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-        <Footer />
-        <Toaster position='top-right' />
+      </div>
+      <Footer />
+      <Toaster position='top-right' />
     </>
   );
 }
