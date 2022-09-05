734e4dedff309f1a7d5e1deb06caa7bd6cfa5444
Gurden Batra <gurden.batra@gmail.com>
2022-09-05 18:05:35 +0300

    update utils with comments and remove VDL

--- src/utils/saf_utils.js
+++ src/utils/saf_utils.js
@@ -1,7 +1,6 @@
 import streetTreeImage from '../images/street-trees.png';
 import urbanParksImage from '../images/urban-park.png';
 import woodlandsImage from '../images/woodland.png';
-import vdlImage from '../images/VDL.png';
 
 const typologies_names = [
   {
@@ -9,24 +8,23 @@ const typologies_names = [
     title: 'Street Trees',
     description: 'A street tree is any tree growing within the public-right-of-way',
     value: 'park',
-
     species: 'conifer',
     image: streetTreeImage,
     costLow: 2262,
     costMed: 2397,
     costHigh: 2532,
-    imperviousPercent: 90,
-    perviousPercent: 10,
-    minDensity: 10,
-    maxDensity: 30,
-    evergreenPercent: 60,
-    decidiousPercent: 40,
+    imperviousPercent: 90,  // not in use, delete
+    perviousPercent: 10,  // not in use, delete
+    minDensity: 10, // autofill median!
+    maxDensity: 30, 
+    evergreenPercent: 60,  // not in use, delete
+    decidiousPercent: 40,  // not in use, delete
     fixedDBH: 7,
     minDBH: 7,
     maxDBH: 15,
-    minHeight: 5,
-    maxHeight: 12,
-    maintenance_type: 2,
+    minHeight: 5,  // not in use, delete
+    maxHeight: 12,  // not in use, delete
+    maintenance_type: 2, // not in use, delete
   },
   {
     id: 1,
@@ -75,31 +73,6 @@ const typologies_names = [
     minHeight: 5,
     maxHeight: 14,
     maintenance_type: 2,
-  },
-
-  {
-    id: 3,
-    title: 'Trees in Vacant Lands',
-    description:
-      'Degraded lands present potential locations for tree plantations and tree preservation',
-    value: 'tree in VDL',
-    species: 'conifer',
-    image: vdlImage,
-    costLow: 12.85,
-    costMed: 12.85,
-    costHigh: 12.85,
-    imperviousPercent: 10,
-    perviousPercent: 90,
-    minDensity: 50,
-    maxDensity: 2000,
-    evergreenPercent: 80,
-    decidiousPercent: 20,
-    fixedDBH: 4.77,
-    minDBH: 7,
-    maxDBH: 30,
-    minHeight: 2,
-    maxHeight: 6,
-    maintenance_type: 1,
   },
 ];
 
