f515ed0d182493c5779dc72629d95e575bfae768
Gurden Batra <gurden.batra@gmail.com>
2022-09-06 22:32:21 +0300

    Add map filters

---
+++ src/utils/map_filters.js
@@ -0,0 +1,60 @@
+const districts = [
+  'Auchenshuggle and Tollcross',
+  'Baillieston',
+  'Barrhead',
+  'Blairdardie and Old Drumchapel',
+  'Bridgeton and Dalmarnock',
+  'Carmunnock',
+  'Carmyle',
+  'Castlemilk',
+  'Cranhill',
+  'Croftfoot and Menock',
+  'Crosshill and Govanhill',
+  'Dundasvale',
+  'Drumchapel',
+  'Garthamlock, Craigend and Gartloch',
+  'Govan East',
+  'High Knightswood and Anniesland',
+  'Hillington, North Cardonald and Penilee',
+  'Kelvindale',
+  'Knightswood',
+  'Levern and District',
+  'Maryhill and Summerston',
+  'Milngavie',
+  'Pollok North',
+  'Possilpark',
+  'Robroyston',
+  'Ruchill',
+  'Sighthill, Royston and Germiston',
+  'Springburn',
+  'Wallacewell',
+  'West Durbanshire',
+  'Yoker',
+];
+
+const typologies = [
+  'Detention basin',
+  'Parkhead',
+  'Raingardens',
+  'Raingardens, Detention basin',
+  'Raingardens, Swales, Infiltation systems',
+  'Raingardens, Swales, Permeable surface, Detention basin',
+  'River restoration',
+  'Sediment removal',
+  'Street trees',
+  'Swales',
+  'Swales, Detention basin',
+  'Swales, Raingardens, Retention pond',
+  'Trees in Vacant or Derelict Land',
+  'Water butts',
+  'Wetland',
+  'Woodland',
+];
+
+export function get_districts() {
+  return districts;
+}
+
+export function get_typologies() {
+  return typologies;
+}
