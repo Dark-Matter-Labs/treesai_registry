0936ca1a97cb2ee81a7a2df75e5007cd79536433
Gurden Batra <gurden.batra@gmail.com>
2022-09-07 21:20:59 +0300

    Improve panel design

--- src/components/map/ProjectsPanel.js
+++ src/components/map/ProjectsPanel.js
@@ -18,51 +18,121 @@ export default function ProjectsPanel(props) {
         leaveFrom='translate-x-0'
         leaveTo='translate-x-full'
       >
-        <div className='px-4 py-10 bg-green-600 rounded-tr-full flex flex-row items-center'>
+        <div className='px-4 py-4 bg-green-600 flex flex-row items-center'>
           <span className='bold-intro-md uppercase text-white-200 pl-10'>NbS Atlas</span>
+
           <button onClick={() => props.setShowProjectPanel(false)}>
-            <ArrowCircleLeftIcon className='text-white-200 w-7 h-7 ml-2' />
+            <ArrowCircleLeftIcon className='text-white-200 w-7 h-7 ml-40' />
           </button>
         </div>
+        <div className='px-10 py-4 text-dark-wood-700 medium-intro-sm '>
+          <p>
+            A database of all submitted and eligible Nature-based Solutions projects in the TreesAI
+            platform globally
+          </p>
+        </div>
+        <div className='px-4 py-2'>
+          <p className='text-green-600 bold-intro-md'>Featured Projects</p>
+          <ul role='list' className='overflow-scroll h-72'>
+            {props.data.map((project) => {
+              if (project.properties.is_featured) {
+                return (
+                  <li
+                    key={project.properties.id}
+                    className={classNames(
+                      project.properties.is_featured ? 'border-green-600' : 'border-dark-wood-600',
+                      'bg-white-300 relative  py-5 px-4 hover:bg-gray-50 rounded-full border my-4 mx-2',
+                    )}
+                    onClick={() => {
+                      props.selectProject(project);
+                    }}
+                  >
+                    <div className='flex justify-between space-x-3'>
+                      <div className='min-w-0 flex-1'>
+                        <a href='#' className='block focus:outline-none'>
+                          <span className='absolute inset-0' aria-hidden='true' />
+                          <p className='medium-intro-sm text-green-600 truncate'>
+                            {project.properties.project_name}
+                          </p>
+                          <p className='medium-intro-sm text-gray-500 truncate'>
+                            {project.properties.community_council}
+                          </p>
+                        </a>
+                      </div>
+                    </div>
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        </div>
+        <div className='px-4'>
+          <p className='text-green-600 bold-intro-md'>Other Projects</p>
 
-        <ul role='list' className='overflow-scroll h-screen'>
-          {props.data.map((project) => (
-            <li
-              key={project.properties.id}
-              className={classNames(
-                project.properties.is_featured ? 'bg-green-100' : 'bg-white-300',
-                'relative  py-5 px-4 hover:bg-gray-50 rounded-full border border-green-600 my-4 mx-2',
-              )}
-              onClick={() => {
-                props.selectProject(project);
-              }}
+          <ul role='list' className='overflow-scroll h-48'>
+            {props.data.map((project) => (
+              <li
+                key={project.properties.id}
+                className={classNames(
+                  project.properties.is_featured ? 'border-green-600' : 'border-dark-wood-600',
+                  'bg-white-300 relative  py-5 px-4 hover:bg-gray-50 rounded-full border my-4 mx-2',
+                )}
+                onClick={() => {
+                  props.selectProject(project);
+                }}
+              >
+                <div className='flex justify-between space-x-3'>
+                  <div className='min-w-0 flex-1'>
+                    <a href='#' className='block focus:outline-none'>
+                      <span className='absolute inset-0' aria-hidden='true' />
+                      <p className='medium-intro-sm text-green-600 truncate'>
+                        {project.properties.project_name}
+                      </p>
+                      <p className='medium-intro-sm text-gray-500 truncate'>
+                        {project.properties.community_council}
+                      </p>
+                    </a>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className='px-4 py-4'>
+          <button
+            type='button'
+            className='bold-intro-sm inline-flex justify-center rounded-full border border-transparent bg-green-600 py-2 px-12 text-white-200 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+          >
+            Add your project →
+          </button>
+          <p className='text-indigo-600 bold-intro-md pt-2'>Featured portfolios</p>
+          <div className='bg-white-300 relative  py-5 px-4 hover:bg-gray-50 rounded-full border border-indigo-600 my-4 mx-2'>
+            <div className='flex justify-between space-x-3'>
+              <div className='min-w-0 flex-1'>
+                <a className='block focus:outline-none'>
+                  <span className='absolute inset-0' aria-hidden='true' />
+                  <p className='medium-intro-sm text-indigo-600 truncate'>TreesAI in Glasgow</p>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className='flex flex-row gap-4'>
+            <button
+              type='button'
+              className='bold-intro-sm inline-flex justify-center rounded-full border border-transparent bg-dark-wood-800 py-2 px-8 text-white-200 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
             >
-              <div className='flex justify-between space-x-3'>
-                <div className='min-w-0 flex-1'>
-                  <a href='#' className='block focus:outline-none'>
-                    <span className='absolute inset-0' aria-hidden='true' />
-                    <p className='medium-intro-md text-gray-900 truncate'>
-                      {project.properties.project_name}
-                    </p>
-                    <p className='medium-intro-sm text-gray-500 truncate'>
-                      {project.properties.stage}
-                    </p>
-                  </a>
-                </div>
-              </div>
-              <div className='mt-1'>
-                <p className='line-clamp-2 medium-intro-sm text-green-600'>
-                  {project.properties.typology}
-                </p>
-              </div>
-              {project.properties.is_featured && (
-                <div className='mt-1'>
-                  <p className='line-clamp-2 medium-intro-sm text-green-800'>Featured</p>
-                </div>
-              )}
-            </li>
-          ))}
-        </ul>
+              Invest →
+            </button>
+
+            <button
+              type='button'
+              className='bold-intro-sm inline-flex justify-center rounded-full border border-transparent bg-indigo-600 py-2 px-8 text-white-200 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+            >
+              Glasgow pilot →
+            </button>
+          </div>
+        </div>
       </Transition.Child>
     </Transition.Root>
   );
