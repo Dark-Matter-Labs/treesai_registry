fb6326a56c85a9378a08a67f103eb4f59bc93d84
Gurden Batra <gurden.batra@gmail.com>
2022-09-14 19:53:10 +0300

    Fix breadcrumb

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,37 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const pages = [
-  { name: 'Upload your project', href: '#', current: true },
-  { name: 'Project Information', href: '#', current: true },
-];
-
-export default function Breadcrumb() {
+export default function Breadcrumb(props) {
   return (
-    <nav className='flex' aria-label='Breadcrumb'>
-      <ol role='list' className='mt-5 flex items-center space-x-4'>
-        {pages.map((page) => (
-          <li key={page.name}>
-            <div className='flex items-center'>
-              <a
-                href={page.href}
-                className='mr-4 text-sm font-medium text-gray-500 hover:text-gray-700'
-                aria-current={page.current ? 'page' : undefined}
-              >
-                {page.name}
-              </a>
-              <svg
-                className='h-5 w-5 flex-shrink-0 text-gray-300'
-                xmlns='http://www.w3.org/2000/svg'
-                fill='currentColor'
-                viewBox='0 0 20 20'
-                aria-hidden='true'
-              >
-                <path d='M5.555 17.776l8-16 .894.448-8 16-.894-.448z' />
-              </svg>
-            </div>
-          </li>
-        ))}
+    <nav className='flex bg-white-200 rounded-full shadow px-8 py-4 my-4' aria-label='Breadcrumb'>
+      <ol role='list' className='flex items-center space-x-4'>
+        <li>
+          <div className='flex items-center'>
+            <span className='mr-4 book-info-md text-dark-wood-600'>Impact Explorer</span>
+            <svg
+              className='h-5 w-5 flex-shrink-0 text-dark-wood-300'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='currentColor'
+              viewBox='0 0 20 20'
+              aria-hidden='true'
+            >
+              <path d='M5.555 17.776l8-16 .894.448-8 16-.894-.448z' />
+            </svg>
+          </div>
+        </li>
+        <li>
+          <div className='flex items-center'>
+            <span className='mr-4 book-info-md text-dark-wood-800 hover:text-gray-700'>
+              {props.title}
+            </span>
+          </div>
+        </li>
       </ol>
     </nav>
   );
 }
+
+Breadcrumb.propTypes = {
+  title: PropTypes.string,
+};
