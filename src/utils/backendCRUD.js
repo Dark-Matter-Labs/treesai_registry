f41c927dea300d1e83f67907c0ae6687272c3111
Gurden Batra <gurden.batra@gmail.com>
2022-11-14 14:35:40 +0200

    Add function to get SAF runs and function to combine both projects and runs

--- src/utils/backendCRUD.js
+++ src/utils/backendCRUD.js
@@ -197,3 +197,40 @@ export const getUserProjects = async (user_id) => {
       console.log('error', error);
     });
 };
+
+export const get_saf_runs_by_projectID = async (project_id) => {
+  const user_id = sessionStorage.user_id;
+
+  const requestHeaders = {
+    accept: 'application/json',
+  };
+
+  let url =
+    process.env.REACT_APP_API_ENDPOINT +
+    '/api/v1/saf/users/' +
+    user_id +
+    '/projects/' +
+    project_id +
+    '/runs/';
+
+  let config = {
+    method: 'GET',
+    headers: requestHeaders,
+    redirect: 'follow',
+  };
+
+  return await axios.get(url, config).then((res) => res.data['runs']);
+};
+
+
+export const get_all_user_runs = async (projectList) => {
+  // Initialize empty array to store all runs
+  let allRuns = [];
+  // Loop through each project and get all runs
+  for (let i = 0; i < projectList.projects.length; i++) {
+    let runs = await get_saf_runs_by_projectID(projectList.projects[i].id);
+    allRuns = allRuns.concat(runs);
+  }
+  // Return all runs
+  return allRuns;
+};
