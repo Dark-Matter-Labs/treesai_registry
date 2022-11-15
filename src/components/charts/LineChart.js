57b195d79dea5da379837ee60cc99a2157e5e8aa
Axel Nilsson <33802893+Axeln78@users.noreply.github.com>
2022-11-15 12:30:17 -0800

    better looking chart

--- src/components/charts/LineChart.js
+++ src/components/charts/LineChart.js
@@ -10,12 +10,58 @@ const LineChart = (props) => {
   }, [props.data]);
 
   return (
-    <div style={{ height: '400px' }}>
+    <div style={{ height: '500px' }}>
       <ResponsiveLine
         data={data}
         margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
         curve='basis'
         enableArea={true}
+        pointColor={{ from: 'color', modifiers: [] }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: 'serieColor' }}
+        axisBottom={{
+          orient: 'bottom',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'Year',
+          legendOffset: 36,
+          legendPosition: 'middle',
+        }}
+        axisLeft={{
+          orient: 'left',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'count',
+          legendOffset: -40,
+          legendPosition: 'middle',
+        }}
+        legends={[
+          {
+            anchor: 'right',
+            direction: 'column',
+            justify: false,
+            translateX: 120,
+            translateY: 0,
+            itemWidth: 100,
+            itemHeight: 20,
+            itemsSpacing: 4,
+            symbolSize: 20,
+            symbolShape: 'circle',
+            itemDirection: 'left-to-right',
+            itemTextColor: '#777',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemBackground: 'rgba(0, 0, 0, .03)',
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
+        ]}
       />
       ;
     </div>
