348b69cbb931dd1bc95b7949adf6c2dfd427d626
Gurden Batra <gurden.batra@gmail.com>
2022-11-21 17:21:17 +0200

    remove data points not in use to avoid confusion

--- src/utils/saf_utils.js
+++ src/utils/saf_utils.js
@@ -10,21 +10,9 @@ const typologies_names = [
     value: 'individual_street_trees',
     species: 'conifer',
     image: streetTreeImage,
-    costLow: 2262,
-    costMed: 2397,
-    costHigh: 2532,
-    imperviousPercent: 90, // not in use, delete
-    perviousPercent: 10, // not in use, delete
-    minDensity: 10, // autofill median!
-    maxDensity: 30,
-    evergreenPercent: 60, // not in use, delete
-    decidiousPercent: 40, // not in use, delete
     fixedDBH: 7,
     minDBH: 7,
     maxDBH: 15,
-    minHeight: 5, // not in use, delete
-    maxHeight: 12, // not in use, delete
-    maintenance_type: 2, // not in use, delete
   },
   {
     id: 1,
@@ -34,21 +22,9 @@ const typologies_names = [
     value: 'park',
     species: 'conifer',
     image: urbanParksImage,
-    costLow: 2262,
-    costMed: 2397,
-    costHigh: 2532,
-    imperviousPercent: 10,
-    perviousPercent: 90,
-    minDensity: 50,
-    maxDensity: 250,
-    evergreenPercent: 60,
-    decidiousPercent: 40,
     fixedDBH: 4.77,
     minDBH: 7,
     maxDBH: 15,
-    minHeight: 5,
-    maxHeight: 40,
-    maintenance_type: 2,
   },
   {
     id: 2,
@@ -58,21 +34,9 @@ const typologies_names = [
     value: 'forest',
     species: 'conifer',
     image: woodlandsImage,
-    costLow: 406,
-    costMed: 483,
-    costHigh: 514,
-    imperviousPercent: 0,
-    perviousPercent: 100,
-    minDensity: 1000,
-    maxDensity: 1600,
-    evergreenPercent: 100,
-    decidiousPercent: 0,
     fixedDBH: 4.77,
     minDBH: 4.77,
     maxDBH: 15,
-    minHeight: 5,
-    maxHeight: 14,
-    maintenance_type: 2,
   },
 ];
 
@@ -96,7 +60,7 @@ const maintenance_types = [
     title: '1 x week',
     description: 'WIP',
     value: 2,
-    id: 2
+    id: 2,
   },
 ];
 
