1630d4556fe354962d8c1b9f6b63fb2a5b381a46
Axel Nilsson <33802893+Axeln78@users.noreply.github.com>
2022-11-21 11:20:10 -0800

    Add more information

--- src/utils/SDGs_helper.js
+++ src/utils/SDGs_helper.js
@@ -1,4 +1,4 @@
-function getSDGIdsFromTypology(typology = 'street tree') {
+export function getSDGIdsFromTypology(typology = 'street tree') {
   // function to get the SDGs from the typology
   switch (typology) {
     case 'Urban Parks':
@@ -7,5 +7,35 @@ function getSDGIdsFromTypology(typology = 'street tree') {
       return [11, 8, 12, 13];
     case 'Woodland':
       return [11, 8, 13];
+  }
+}
+
+function getSDGDetailsFromId(SDGId) {
+  // function to get the SDG details from the SDG ID
+  switch (SDGId) {
+    case 11:
+      return {
+        name: 'Sustainable Cities and Communities',
+        description: 'WIP',
+        // image: SDG11Image,
+      };
+    case 8:
+      return {
+        name: 'Decent Work and Economic Growth',
+        description: 'WIP',
+        // image: SDG8Image,
+      };
+    case 12:
+      return {
+        name: 'Responsible Consumption and Production',
+        description: 'WIP',
+        // image: SDG12Image,
+      };
+    case 13:
+      return {
+        name: 'Climate Action',
+        description: 'WIP',
+        // image: SDG13Image,
+      };
   }
 }
