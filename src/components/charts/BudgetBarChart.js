90d7ba98cb6844ffc5e85a7b6bfd9588cf0b7a5e
Axel Nilsson <33802893+Axeln78@users.noreply.github.com>
2022-11-23 11:10:31 -0800

    First draft of the budget chart

---
+++ src/components/charts/BudgetBarChart.js
@@ -0,0 +1,44 @@
+import { ResponsiveBar } from '@nivo/bar';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const BarChart = (props) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(props.data);
+  }, [props.data]);
+
+  return (
+    <div style={{ height: '400px' }}>
+      <ResponsiveBar
+        data={data}
+        keys={['budget']}
+        indexBy='id'
+        groupMode='grouped'
+        padding={0.05}
+        innerPadding={1}
+        margin={{ top: 80, right: 20, bottom: 60, left: 40 }}
+        axisBottom={{
+          legend: 'YEARS RANGES',
+          legendOffset: 40,
+        }}
+        colors={['#1EB792', '#2F3130', '#4F46E5']}
+        colorBy='id'
+        theme={{
+          background: '#FCFCFC',
+          textColor: '#4F46E5',
+        }}
+        enableGridX={false}
+        enableGridY={false}
+        enableLabel={false}
+      />
+    </div>
+  );
+};
+
+BarChart.propTypes = {
+  data: PropTypes.array,
+};
+
+export default BarChart;
