cb981fa1e603cb4a6d43b19bb4229ddb33b62f00
Gurden Batra <gurden.batra@gmail.com>
2022-11-24 12:57:12 +0200

    Update z index to be lower than table and update placement

--- src/components/map/DataLayerSelector.js
+++ src/components/map/DataLayerSelector.js
@@ -10,7 +10,7 @@ const layers = [
 
 export default function LayerSelector(props) {
   return (
-    <div className='absolute z-50 top-80 right-0 mx-5'>
+    <div className='absolute z-10 top-2/4 right-0 mx-5'>
       <label htmlFor='map-layers' className='book-info-md text-dark-wood-200'>
         Map Layers
       </label>
