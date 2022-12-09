1a2747e22c9f0aad06bd87a011b47a2b23243ef6
Gurden Batra <gurden.batra@gmail.com>
2022-12-08 17:11:23 -0800

    Create util to hold all little eye texts

---
+++ src/utils/slide_over_texts.js
@@ -0,0 +1,10 @@
+const slide_texts = {
+  projectLength:
+    '<p>While we hope your project will flourish for years, <span class="bold-intro-md">decades</span> (and even centuries!) to come - in order to understand better the costs, please let us know how many months your request for funding is meant to cover.</p>',
+  projectStage:
+    '<p>To better understand the NbS projects and set a timeline of activities, Trees AI has defined the 7 stages that these projects go through based on the <u>RIBA Plan of Work</u></p><p class="mt-2"><ul><li>1 Strategic Development: definition of a project, the preparation and briefing</li><li>2 Pre-planning Application: concept design and spatial coordination</li></ul></p>',
+};
+
+export function get_slide_texts() {
+  return slide_texts;
+}
