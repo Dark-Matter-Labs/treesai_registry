52861c022a5f05eebcf25bc3cd29d96287b54ab6
Gurden Batra <gurden.batra@gmail.com>
2023-02-09 17:14:25 +0200

    remove links to the demo

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Glasgow from './pages/Glasgow';
 import Invest from './pages/Invest';
 import Learn from './pages/Learn';
 import Contact from './pages/Contact';
-import Demo from './pages/Demo';
 import Account from './pages/Account';
 import NotFound from './pages/NotFound';
 import ScrollToTop from './utils/ScrollToTop';
@@ -28,16 +27,14 @@ function App() {
         <Route exact path='/' element={<Home loggedIn={loggedIn} />} />
         <Route exact path='/develop' element={<Develop loggedIn={loggedIn} />} />
         <Route exact path='/explore' element={<Explore loggedIn={loggedIn} />} />
-        <Route exact path='/demo' element={<Demo />} />
         <Route exact path='/register' element={<Register />} />
         <Route exact path='/login' element={<Login />} />
         <Route exact path='/glasgow-nbs-portfolio' element={<Glasgow />} />
-        <Route exact path='/invest' element={<Invest />} />
-        <Route exact path='/learn-more' element={<Learn />} />
-        <Route exact path='/contact' element={<Contact />} />
-        <Route exact path='/demo' element={<Demo />} />
+        <Route exact path='/invest' element={<Invest loggedIn={loggedIn} />} />
+        <Route exact path='/learn-more' element={<Learn loggedIn={loggedIn} />} />
+        <Route exact path='/contact' element={<Contact loggedIn={loggedIn} />} />
         <Route exact path='/account' element={<Account loggedIn={loggedIn} />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path='*' element={<NotFound loggedIn={loggedIn} />} />
       </Routes>
     </Router>
   );
